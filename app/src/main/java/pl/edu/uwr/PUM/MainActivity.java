0598cb5b627525a4002acabb6285ae68cf031076
dominiklis <lis.dominik11@gmail.com>
2020-06-10 18:47:27 +0200

    new project

---
+++ app/src/main/java/pl/edu/uwr/PUM/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.edu.uwr.PUM;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
