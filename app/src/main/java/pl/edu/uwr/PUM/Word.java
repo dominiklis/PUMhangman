1795f238913e6ae52d5807a6bf7b0844c73a6126
dominiklis <lis.dominik11@gmail.com>
2020-06-11 18:45:33 +0200

    Create Word.java

---
+++ app/src/main/java/pl/edu/uwr/PUM/Word.java
@@ -0,0 +1,18 @@
+package pl.edu.uwr.PUM;
+
+public class Word {
+
+    private int mWordId;
+
+    public Word(int wid) {
+        mWordId = wid;
+    }
+
+    public int getWordId() {
+        return mWordId;
+    }
+
+    public void setWordId(int id) {
+        mWordId = id;
+    }
+}
